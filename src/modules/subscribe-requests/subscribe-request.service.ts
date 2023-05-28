a7d7bdf51903238e10b20eae8dd02b0ef61faca1
hanhnd <hanhnd156@gmail.com>
2023-05-28 13:28:02 +0700

    [Feat] Complete profile page

--- src/modules/subscribe-requests/subscribe-request.service.ts
+++ src/modules/subscribe-requests/subscribe-request.service.ts
@@ -1,5 +1,6 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { DEFAULT_PAGE_LIMIT, DEFAULT_PAGE_VALUE, SubscribeRequestStatus } from 'src/common/constants';
+import { toObjectId } from 'src/common/helper';
 import { IDataServices } from 'src/common/repositories/data.service';
 import { IDataResources } from 'src/common/resources/data.resource';
 import { SubscribeRequest, User } from 'src/mongo-schemas';
@@ -17,7 +18,35 @@ export class SubscribeRequestService {
                 status: SubscribeRequestStatus.PENDING,
             },
             {
-                populate: ['receiver'],
+                populate: [
+                    {
+                        path: 'sender',
+                        select: '_id fullName avatarId',
+                    },
+                ],
+                skip: skip,
+                limit: +limit,
+                sort: [['createdAt', -1]],
+            },
+        );
+        return subscribeRequests;
+    }
+
+    async getSentSubscribeRequests(loginUser: User, query: IGetSubscribeRequestListQuery) {
+        const { page = DEFAULT_PAGE_VALUE, limit = DEFAULT_PAGE_LIMIT } = query;
+        const skip = (+page - 1) * +limit;
+        const subscribeRequests = await this.dataServices.subscribeRequests.findAll(
+            {
+                sender: loginUser._id,
+                status: SubscribeRequestStatus.PENDING,
+            },
+            {
+                populate: [
+                    {
+                        path: 'receiver',
+                        select: '_id fullName avatarId',
+                    },
+                ],
                 skip: skip,
                 limit: +limit,
                 sort: [['createdAt', -1]],
@@ -41,12 +70,11 @@ export class SubscribeRequestService {
         const { status } = body;
 
         const toUpdateSubscribeRequest = await this.dataServices.subscribeRequests.findOne({
-            receiver: user._id,
-            _id: subscribeRequestId,
+            _id: toObjectId(subscribeRequestId),
             status: SubscribeRequestStatus.PENDING,
         });
 
-        if (toUpdateSubscribeRequest) {
+        if (!toUpdateSubscribeRequest) {
             throw new BadRequestException(`Không tìm thấy lời mời này.`);
         }
 
@@ -56,7 +84,37 @@ export class SubscribeRequestService {
                 status,
             },
             {
-                populate: ['sender', 'receiver'],
+                populate: [
+                    {
+                        path: 'sender',
+                        select: '_id fullName avatarId subscriberIds subscribingIds',
+                    },
+                    {
+                        path: 'receiver',
+                        select: '_id fullName avatarId subscriberIds subscribingIds',
+                    },
+                ],
+            },
+        );
+
+        return updatedSubscribeRequest;
+    }
+
+    async cancelSubscribeRequest(user: User, targetUser: User) {
+        const toUpdateSubscribeRequest = await this.dataServices.subscribeRequests.findOne({
+            sender: user._id,
+            receiver: targetUser._id,
+            status: SubscribeRequestStatus.PENDING,
+        });
+
+        if (!toUpdateSubscribeRequest) {
+            return null;
+        }
+
+        const updatedSubscribeRequest = await this.dataServices.subscribeRequests.updateById(
+            toUpdateSubscribeRequest._id,
+            {
+                status: SubscribeRequestStatus.REJECTED,
             },
         );
 
