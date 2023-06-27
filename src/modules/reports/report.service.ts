2abfac83b8c1766fe3c546dc7c6c56f4f5e4be03
hanhnd <hanhnd156@gmail.com>
2023-06-27 22:03:22 +0700

    update ui

--- src/modules/reports/report.service.ts
+++ src/modules/reports/report.service.ts
@@ -1,5 +1,5 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { ReportAction } from 'src/common/constants';
+import { ReportAction, SystemReporter } from 'src/common/constants';
 import { ReportTarget } from 'src/common/interfaces';
 import { IDataServices } from 'src/common/repositories/data.service';
 import { IDataResources } from 'src/common/resources/data.resource';
@@ -10,26 +10,30 @@ import { ICreateReportBody } from './report.interface';
 export class ReportService {
     constructor(private dataServices: IDataServices, private dataResources: IDataResources) {}
 
-    async create(user: User, targetType: string, target: ReportTarget, body: ICreateReportBody) {
-        const existedReport = await this.dataServices.reports.findOne({
-            author: user._id,
-            targetType,
-            target: target._id,
-            action: {
-                $in: [ReportAction.PENDING, ReportAction.IN_PROGRESS],
-            },
-        });
-        if (existedReport) {
-            throw new BadRequestException(`Yêu cầu của bạn đang được xem xét.`);
+    async create(user: User | SystemReporter, targetType: string, target: ReportTarget, body: ICreateReportBody) {
+        if ((user as User)?._id) {
+            const existedReport = await this.dataServices.reports.findOne({
+                author: (user as User)._id,
+                targetType,
+                target: target._id,
+                action: {
+                    $in: [ReportAction.PENDING, ReportAction.IN_PROGRESS],
+                },
+            });
+
+            if (existedReport) {
+                throw new BadRequestException(`Yêu cầu của bạn đang được xem xét.`);
+            }
         }
 
         const { reportReason } = body;
         const toCreateReportBody: Partial<Report> = {
-            author: user._id as unknown,
+            author: (user as User)?._id ?? (null as unknown),
             target: target._id as unknown,
             targetType: targetType,
             reportReason: reportReason,
             action: ReportAction.PENDING,
+            systemReporter: (user as User)?._id ? null : (user as SystemReporter),
         };
 
         const createdReport = await this.dataServices.reports.create(toCreateReportBody);
