93b4e028e693873d6b20dc85dda606b782552d42
hanhnd <hanhnd156@gmail.com>
2023-06-27 21:38:10 +0700

    fix bug

--- src/modules/cron-jobs/cron-job.module.ts
+++ src/modules/cron-jobs/cron-job.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
 import { RedisModule } from 'src/common/modules/redis/redis.module';
 import { DataServicesModule } from 'src/common/repositories/data-services.module';
-import { NotificationModule } from '../notifications/notification.module';
 import { SystemMessageModule } from '../moderator/system-messages/moderator-system-message.module';
+import { NotificationModule } from '../notifications/notification.module';
 import { HappyBirthdayJob } from './happy-birthday.job';
 import { OnlineAlertJob } from './online-alert.job';
 import { ReducePointJob } from './reduce-point.job';
+import { RemoveLimitUserJob } from './remove-limit-user.job';
 import { SleepReminderJob } from './sleep-reminder.job';
 import { SurveyScanJob } from './survey-scan-job';
 
 @Module({
     imports: [DataServicesModule, RedisModule, NotificationModule, SystemMessageModule],
-    providers: [OnlineAlertJob, HappyBirthdayJob, SleepReminderJob, ReducePointJob, SurveyScanJob],
+    providers: [OnlineAlertJob, HappyBirthdayJob, SleepReminderJob, ReducePointJob, SurveyScanJob, RemoveLimitUserJob],
 })
 export class CronJobModule {}
